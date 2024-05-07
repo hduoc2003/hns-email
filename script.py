370e54950e8fe9db8c935a873ef3c2190ba8e7c4
hduoc2003 <hduoc2003@gmail.com>
2024-05-07 11:14:30 +0700

    update code

--- script.py
+++ script.py
@@ -1,12 +1,11 @@
 import os
 from dotenv import load_dotenv
 import subprocess
-import ruamel.yaml
 
 def get_env(key: str) -> str:
     val = os.getenv(key)
     if val is None:
-        raise Exception("{key} not found in mailu.env")
+        raise Exception("{key} not found in .env")
     return val
 
 load_dotenv('.env')
@@ -15,14 +14,15 @@ HOSTNAMES = get_env("HOSTNAMES")
 STORAGE_PATH = get_env("STORAGE_PATH")
 WEBSITE_NAME = get_env("WEBSITE_NAME")
 IP_ADDRESS = get_env("IP_ADDRESS")
-
-TEMPLATE_DOMAIN = "moon.allinpepetothemoon"
-TEMPLATE_HOSTNAMES = "mail.moon.allinpepetothemoon"
-TEMPLATE_STORAGE_PATH = "/mailu/"
-TEMPLATE_WEBSITE_NAME = "MoonMail"
-TEMPLATE_IP_ADDRESS = "14.225.217.169"
-
 HNS_DNS = "103.196.38.38"
+
+TEMPLATE_DOMAIN = "$DOMAIN"
+TEMPLATE_HOSTNAMES = "$HOSTNAMES"
+TEMPLATE_STORAGE_PATH = "$STORAGE_PATH"
+TEMPLATE_WEBSITE_NAME = "$WEBSITE_NAME"
+TEMPLATE_IP_ADDRESS = "$IP"
+TEMPLATE_HNS_DNS = "$HNS_DNS"
+
 
 # Gen nginx.conf
 def gen_nginx():
@@ -41,8 +41,8 @@ def gen_env():
 
     with open('server.temp.env') as r:
         temp_env = r.read()
-    env = temp_env.replace("$HOSTNAMES", HOSTNAMES) \
-                       .replace("$DOMAIN", DOMAIN) \
+    env = temp_env.replace(TEMPLATE_HOSTNAMES, HOSTNAMES) \
+                       .replace(TEMPLATE_DOMAIN, DOMAIN) \
                        .replace(TEMPLATE_WEBSITE_NAME, WEBSITE_NAME)
     with open('mailu.env', 'w') as w:
         w.write(env)
@@ -61,40 +61,42 @@ def gen_cert():
 def gen_docker_compose():
     print('Generating docker-compose.yml')
 
-    yaml = ruamel.yaml.YAML()
-    yaml.preserve_quotes = True
-    yaml.indent(sequence=3, offset=1)
+    # yaml = ruamel.yaml.YAML()
+    # yaml.preserve_quotes = True
+    # yaml.indent(sequence=3, offset=1)
 
-    # Add configurations
-    with open("docker-compose.temp.yml", 'r') as ymlfile:
-        data = yaml.load(ymlfile)
-    for key, service in data['services'].items():
-        if 'dns' in service and key != 'admin':
-            service['dns'].insert(0, HNS_DNS)
-    data['services']['front']['volumes'] += ["./cert.crt:/etc/ssl/cert.crt:ro",
-                                             "./cert.key:/etc/ssl/cert.key:ro",
-                                             "./nginx.conf:/etc/nginx/nginx1.conf:ro",
-                                             "./tls.conf:/etc/nginx/tls1.conf:ro"]
-    data['services']['front']['command'] = [
-            "/bin/sh",
-            "-c",
-            "/start.py & sleep 20 && cd /etc/nginx/ && rm nginx.conf && cp nginx1.conf nginx.conf && rm tls.conf && cp tls1.conf tls.conf && nginx -s reload && sleep infinity"
-        ]
-    with open('docker-compose.yml', 'w') as w:
-        yaml.dump(data, w)
+    # # Add configurations
+    # with open("docker-compose.temp.yml", 'r') as ymlfile:
+    #     data = yaml.load(ymlfile)
+    # for key, service in data['services'].items():
+    #     if 'dns' in service and key != 'admin':
+    #         service['dns'].insert(0, HNS_DNS)
+    # data['services']['front']['volumes'] += ["./cert.crt:/etc/ssl/cert.crt:ro",
+    #                                          "./cert.key:/etc/ssl/cert.key:ro",
+    #                                          "./nginx.conf:/etc/nginx/nginx1.conf:ro",
+    #                                          "./tls.conf:/etc/nginx/tls1.conf:ro"]
+    # data['services']['front']['command'] = [
+    #         "/bin/sh",
+    #         "-c",
+    #         "/start.py & sleep 20 && cd /etc/nginx/ && rm nginx.conf && cp nginx1.conf nginx.conf && rm tls.conf && cp tls1.conf tls.conf && nginx -s reload && sleep infinity"
+    #     ]
+    # with open('docker-compose.yml', 'w') as w:
+    #     yaml.dump(data, w)
 
     # Replace template
-    with open('docker-compose.yml', 'r') as r:
+    with open('docker-compose.temp.yml', 'r') as r:
         data = r.read()
         data = data.replace(TEMPLATE_IP_ADDRESS, IP_ADDRESS) \
-                        .replace(TEMPLATE_STORAGE_PATH, STORAGE_PATH + '/')
+                   .replace(TEMPLATE_STORAGE_PATH, STORAGE_PATH) \
+                   .replace(TEMPLATE_HNS_DNS, HNS_DNS)
+
     with open('docker-compose.yml', 'w') as w:
         w.write(data)
 
     print('Finish generating docker-compose.yml')
 
 if __name__ == "__main__":
-    # gen_nginx()
-    # gen_cert()
+    gen_nginx()
+    gen_cert()
     gen_env()
-    # gen_docker_compose()
+    gen_docker_compose()
